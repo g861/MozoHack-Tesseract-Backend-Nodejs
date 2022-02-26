53f861ccd122b18c0018e370dcd78072bf9c2a20
g861 <aggarwalgrahil8@gmail.com>
2022-02-26 17:15:44 +0530

    Backend for MOZOHACK

---
+++ models/user.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose') ; 
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type:String, 
+        required : true,
+    },
+    email : {
+        type : String , 
+        required : true ,
+    }, 
+    passwordHash:{
+        type:String , 
+        required : true , 
+    }, 
+    phone : {
+        type: String , 
+        required : true , 
+    },
+    isAdmin : {
+        type: Boolean , 
+        required : true , 
+    },
+    street:{
+        type: String , 
+        default : ''
+    },
+    apartment : {
+        type: String , 
+        default : ''
+    },
+    zip:{
+        type: String , 
+        default : ''
+    },
+    city: {
+        type: String , 
+        default : ''
+    },
+    country: {
+        type: String , 
+        default : ''
+    },
+    coinsEarned : {
+        type : Number ,
+        required : true , 
+        min : 0 
+    }
+})
+
+
+userSchema.virtual('id').get(function(){
+    return this._id.toHexString() ;
+})
+
+userSchema/mongoose.set('toJSON',{
+    virtuals : true ,
+})
+
+exports.User = mongoose.model('User',userSchema) ; 
+exports.userSchema = userSchema ; 
